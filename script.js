501477e0c775be33e89d04d7258c6855cde62c81
k.kashimoto <k.kashimoto@globis.co.jp>
2024-09-10 00:46:17 +0900

    final

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 $(document).ready(function() {
-    const topics = ["お題1", "お題2", "お題3", "お題4"];
+    const topics = ["「直近の悩み」を話せ", "「QAの未来」について語れ",  "「GDPの未来」を語れ", "「これはハラスメント？」を語れ"];
     let count = 0;
     let lastClickTime = 0;  // 最後にボタンを押した時間を記録
 
     // お題ボタンを押した時の処理
     $(".topicButton").on("click", function() {
+        console.log("button pressed");
         const topicIndex = $(this).index();
         $("#topicDisplay").text(topics[topicIndex]);
-        $(".speechButton").removeClass('done');  // 全ボタンのリセット
-        count = 0;
-        $("#celebration").hide(); // クリアエフェクトを隠す
+        resetState();  // 状態をリセット
     });
 
-   // 発言ボタンを押した時の処理
-   $(".speechButton").on("click", function() {
+    // 発言ボタンを押した時の処理
+    $(".speechButton").on("click", function() {
         const currentTime = Date.now();  // 現在の時間をミリ秒で取得
         const timeDifference = currentTime - lastClickTime;
 
@@ -36,17 +35,13 @@ $(document).ready(function() {
 
     function showCelebration() {
         setTimeout(function() {
-            showCelebration2();
+            $("#celebration").show().text("ミッションクリア！");
+            $("#fanfare")[0].play();  // ファンファーレの音を再生
+            setTimeout(function() {
+                $("#celebration").fadeOut(); // 3秒後にフェードアウト
+                resetState();  // 状態を初期化
+            }, 5000);
         }, 5000);
-    }
-
-    function showCelebration2() {
-        $("#celebration").show().text("ミッションクリア！");
-        $("#fanfare")[0].play();  // ファンファーレの音を再生
-        setTimeout(function() {
-            $("#celebration").fadeOut(); // 3秒後にフェードアウトする
-        }, 3000);
-
     }
 
     function showPenalty() {
@@ -56,4 +51,12 @@ $(document).ready(function() {
             $("#penalty").fadeOut();  // 3秒後にフェードアウト
         }, 6000);
     }
- });
+
+    // 状態をリセットする関数
+    function resetState() {
+        $(".speechButton").removeClass('done');  // ボタンの色を初期化
+        // $("#topicDisplay").text("お題");  // お題表示を初期状態に戻す
+        count = 0;  // カウントリセット
+        lastClickTime = 0;  // タイマーリセット
+    }
+});
